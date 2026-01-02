f50c1c17cfb83aee6e84ec2c48f6393a1dcc5abf
Juyoung Kim <juyoungk23@gmail.com>
2026-01-02 09:59:06 -0800

    Initial Commit

---
+++ Lumina/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lumina
+//
+//  Created by Juyoung Kim on 1/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lumina/LuminaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuminaApp.swift
+//  Lumina
+//
+//  Created by Juyoung Kim on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct LuminaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
